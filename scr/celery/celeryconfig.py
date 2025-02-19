d888d75b394e6125bf2d9730c7444d8fe640284c
Sustavov Maksim <maksim.sustavov@edgecenter.ru>
2025-02-19 20:15:18 +0500

    del task add

--- scr/celery/celeryconfig.py
+++ scr/celery/celeryconfig.py
@@ -10,11 +10,6 @@ def get_celery_config() -> dict:
         enable_utc=True,
         include=["scr.celery.tasks"],
         beat_schedule={
-            "add-every-5-seconds": {
-                "task": "scr.celery.tasks.add",
-                "schedule": 2.0,
-                "args": (16, 16),
-            },
             "add-every-1-minute": {
                 "task": "scr.celery.tasks.check_youtube_channel",
                 "schedule": 60.0,
