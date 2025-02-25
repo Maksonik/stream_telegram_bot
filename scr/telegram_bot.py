86fa0fa71e5a1fd2990a7cc3c6f3f4518066c3e6
Sustavov Maksim <maksim.sustavov@edgecenter.ru>
2025-02-25 14:49:27 +0500

    rewrite if

--- scr/telegram_bot.py
+++ scr/telegram_bot.py
@@ -22,7 +22,7 @@ class TelegramBot:
         self.bot = telegram.Bot(os.environ["TELEGRAM_TOKEN"])
         self.chat_id = os.environ["CHANNEL"]
 
-    async def send_message(self, title, url) -> None:
+    async def send_message(self, title, url, has_15_minutes_notice: bool = False) -> None:
         """
         Send a message about a scheduled stream video
         :param title: title of video
@@ -30,9 +30,11 @@ class TelegramBot:
         :return: None
         """
         logging.info(msg=f"Creating message, DICT_MESSAGES={self.LIST_MESSAGES}, TelegramBot={id(self)}")
-        if title not in self.LIST_MESSAGES:
+        if any(filter(lambda x: x.title != title and x.has_15_minutes_notice != has_15_minutes_notice, self.LIST_MESSAGES)):
             message = await self.bot.send_message(chat_id=self.chat_id, text=f"I'm a bot, please talk to me! {url}")
-            self.LIST_MESSAGES.append(DataMessage(title=title, id=message.message_id))
+            self.LIST_MESSAGES.append(DataMessage(title=title,
+                                                  id=message.message_id,
+                                                  has_15_minutes_notice=has_15_minutes_notice))
             logging.info(msg=f"Created message, id={message.message_id}, DICT_MESSAGES={self.LIST_MESSAGES}")
 
     async def delete_message(self) -> None:
