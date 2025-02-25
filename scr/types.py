41e821c511540c7914bffb8d8410e918e530a103
Sustavov Maksim <maksim.sustavov@edgecenter.ru>
2025-02-25 16:33:19 +0500

    fix DataVideo

--- scr/types.py
+++ scr/types.py
@@ -3,9 +3,9 @@ from dataclasses import dataclass
 
 @dataclass
 class DataVideo:
-    title: str
-    url_video: str
-    time_scheduled_video: str
+    title: str = ""
+    url_video: str = ""
+    time_scheduled_video: str = ""
 
 
 @dataclass
