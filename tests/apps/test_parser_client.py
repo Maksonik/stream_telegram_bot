8559c38d079c6a956e9ece78b9d049dcef170785
Sustavov Maksim <maksim.sustavov@edgecenter.ru>
2025-02-25 21:03:08 +0500

    create test for telegram bot

--- tests/apps/test_parser_client.py
+++ tests/apps/test_parser_client.py
@@ -6,7 +6,7 @@ from scr.types import DataVideo
 
 
 @pytest.mark.vcr
-def test_parser(page_without_scheduled_stream_video):
+def test_parser_client(page_without_scheduled_stream_video):
     parser = ParserYouTube(settings=get_settings())
     data = parser.get_information()
     assert data == DataVideo(title='От Джуна до Мидла | День 11', url_video='/watch?v=-_j_EboM3Bs', time_scheduled_video='Streamed 3 weeks ago')
---
+++ tests/apps/test_telegram_client.py
@@ -0,0 +1,17 @@
+import pytest
+
+from scr.apps.telegram_client import TelegramBot
+from scr.core.settings import get_settings
+from scr.types import DataVideo
+
+
+@pytest.mark.vcr(record_mode="once")
+async def test_telegram_client():
+    telegram_bot = TelegramBot(settings=get_settings())
+    await telegram_bot.send_message(title="Test message", url="test_url")
+
+    assert len(telegram_bot.LIST_MESSAGES) == 1
+
+    await telegram_bot.delete_message()
+
+    assert len(telegram_bot.LIST_MESSAGES) == 0
