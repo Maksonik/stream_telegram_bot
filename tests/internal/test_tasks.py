4cecc9921f54e013aa227800ea42fbd33016cda4
Sustavov Maksim <maksim.sustavov@edgecenter.ru>
2025-02-26 20:05:25 +0500

    fix test for celery

--- tests/internal/test_tasks.py
+++ tests/internal/test_tasks.py
@@ -2,5 +2,5 @@ from scr.internal.tasks import task_healthcheck
 
 
 def test_add(celery_app):
-    result = task_healthcheck.delay()  # Запускаем задачу
+    result = task_healthcheck.delay()
     assert result.get(timeout=10) == {"status": "ok"}
